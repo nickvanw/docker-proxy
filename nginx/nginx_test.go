cbb51db2628235a39da408aa075d312442510cc5
Nick Van Wiggeren <nick@facepwn.com>
2016-03-28 21:51:52 -0400

    nginx: initial commits of config and nginx tests

---
+++ nginx/nginx_test.go
@@ -0,0 +1,244 @@
+package nginx
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/nickvanw/docker-proxy"
+)
+
+func TestHostNoSSL(t *testing.T) {
+	s, err := New(os.TempDir(), os.TempDir(), "/bin/true")
+	if err != nil {
+		t.Fatalf("unable to create new server: %s", err)
+	}
+	host := "www.google.com"
+	site := dockerproxy.Site{
+		ID:  "my-id-here",
+		Env: map[string]string{},
+	}
+	wantOutput := `
+server {
+	server_name www.google.com;
+	listen 80;
+	 
+	location / {
+		proxy_pass http://my-id-here;
+	}
+}
+`
+
+	buf := bytes.NewBuffer(nil)
+	err = s.renderHost(buf, host, site)
+	if err != nil {
+		t.Fatalf("wanted no error when laying down basic site template, got: %v", err)
+	}
+
+	if buf.String() != wantOutput {
+		t.Fatalf("want: %q\n got: %q\n", wantOutput, buf.String())
+	}
+}
+
+func TestHostWithConfigNoSSL(t *testing.T) {
+	s, err := New(os.TempDir(), os.TempDir(), "/bin/true")
+	if err != nil {
+		t.Fatalf("unable to create new server: %s", err)
+	}
+	host := "www.google.com"
+	site := dockerproxy.Site{
+		ID:  "my-id-here",
+		Env: map[string]string{"NGINX_CLIENT_MAX_BODY_SIZE": "5m"},
+	}
+	wantOutput := `
+server {
+	server_name www.google.com;
+	listen 80;
+	client_max_body_size 5m; 
+	location / {
+		proxy_pass http://my-id-here;
+	}
+}
+`
+
+	buf := bytes.NewBuffer(nil)
+	err = s.renderHost(buf, host, site)
+	if err != nil {
+		t.Fatalf("wanted no error when laying down basic site template, got: %v", err)
+	}
+
+	if buf.String() != wantOutput {
+		t.Fatalf("want: %q\n got: %q\n", wantOutput, buf.String())
+	}
+}
+
+func TestSSLInfo(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "ssltest")
+	if err != nil {
+		t.Fatalf("unable to make temp dir: %q", err)
+	}
+	defer func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			t.Errorf("unable to remove tmp files: %s", err)
+		}
+	}()
+
+	s := &Server{ssl: tmpDir}
+	tt := []struct {
+		makeFile func()
+		host     string
+		site     dockerproxy.Site
+		wantOut  string
+		wantOk   bool
+	}{
+		{
+			makeFile: func() {
+				ioutil.WriteFile(filepath.Join(tmpDir, "google.com.crt"), []byte{}, os.FileMode(0400))
+				ioutil.WriteFile(filepath.Join(tmpDir, "google.com.key"), []byte{}, os.FileMode(0400))
+			},
+			host:    "google.com",
+			site:    dockerproxy.Site{Env: map[string]string{}},
+			wantOut: filepath.Join(tmpDir, "google.com"),
+			wantOk:  true,
+		},
+		{
+			makeFile: func() {},
+			host:     "www.doesnotmatter.com",
+			site:     dockerproxy.Site{Env: map[string]string{}},
+			wantOut:  "",
+			wantOk:   false,
+		},
+		{
+			makeFile: func() {
+				ioutil.WriteFile(filepath.Join(tmpDir, "somekey.crt"), []byte{}, os.FileMode(0400))
+				ioutil.WriteFile(filepath.Join(tmpDir, "somekey.key"), []byte{}, os.FileMode(0400))
+			},
+			host:    "www.doesnotmatch.com",
+			site:    dockerproxy.Site{Env: map[string]string{"CERT_NAME": "somekey"}},
+			wantOut: filepath.Join(tmpDir, "somekey"),
+			wantOk:  true,
+		},
+
+		{
+			makeFile: func() {
+				ioutil.WriteFile(filepath.Join(tmpDir, "almost_matches_both2.crt"), []byte{}, os.FileMode(0400))
+				ioutil.WriteFile(filepath.Join(tmpDir, "almost_matches_both.key"), []byte{}, os.FileMode(0400))
+			},
+			host:    "www.doesnotmatch.com",
+			site:    dockerproxy.Site{Env: map[string]string{"CERT_NAME": "almost_matches_both"}},
+			wantOk:  false,
+			wantOut: "",
+		},
+	}
+	for _, v := range tt {
+		v.makeFile()
+		got, gotOk := s.sslInfo(v.host, v.site)
+		if gotOk != v.wantOk {
+			t.Fatalf("wanted ok: %q, got: %q", v.wantOk, gotOk)
+		}
+
+		if got != v.wantOut {
+			t.Fatalf("wanted: %q, got: %q", v.wantOut, got)
+		}
+	}
+}
+func TestHTTPSSite(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "ssltest")
+	if err != nil {
+		t.Fatalf("unable to make temp dir: %q", err)
+	}
+	defer func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			t.Errorf("unable to remove temp file: %s", err)
+		}
+	}()
+
+	wantOut := `
+server {
+	server_name www.google.com;
+	listen 80;
+	return 301 https://$host$request_uri;
+}
+
+server {
+	server_name www.google.com;
+	listen 443 ssl;
+
+	ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+	ssl_ciphers "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA";
+	ssl_prefer_server_ciphers on;
+	ssl_session_timeout 5m;
+	ssl_session_cache shared:SSL:50m;
+
+	ssl_certificate ssldir/www.google.com.crt;
+	ssl_certificate_key ssldir/www.google.com.key;
+	add_header Strict-Transport-Security "max-age=31536000";
+	 
+	location / {
+		proxy_pass http://my-id-upstream;
+	}
+}
+`
+	s, err := New(tmpDir, tmpDir, "")
+	if err != nil {
+		t.Fatalf("unable to create updater: %q", err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(tmpDir, "www.google.com.crt"), []byte("hi"), os.FileMode(0400)); err != nil {
+		t.Fatalf("unable to write crt file: %q", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmpDir, "www.google.com.key"), []byte("hi"), os.FileMode(0400)); err != nil {
+		t.Fatalf("unable to write key file: %q", err)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	site := dockerproxy.Site{Env: map[string]string{}, ID: "my-id-upstream"}
+	err = s.renderHost(buf, "www.google.com", site)
+	got := buf.String()
+	toCompare := strings.Replace(got, tmpDir, "ssldir", -1)
+	if toCompare != wantOut {
+		t.Fatalf("want: %q\n got: %q", wantOut, toCompare)
+	}
+}
+
+func TestHTTPAuthInfo(t *testing.T) {
+	//dir := os.TempDir()
+
+	tt := []struct {
+		site     string
+		env      map[string]string
+		wantData []byte
+		wantOk   bool
+	}{
+		{
+			site:     "test.com",
+			env:      map[string]string{},
+			wantData: nil,
+			wantOk:   false,
+		},
+		{
+			site:     "test.com",
+			env:      map[string]string{"AUTH_USER": "nick", "AUTH_PASS": "password"},
+			wantData: []byte("nick:password"),
+			wantOk:   true,
+		},
+	}
+
+	for _, v := range tt {
+		s := &Server{}
+		site := dockerproxy.Site{
+			Env: v.env,
+		}
+		info, ok := s.httpAuthInfo(v.site, site)
+		if v.wantOk != ok {
+			t.Fatalf("wanted %v, got: %v from yes/no", v.wantOk, ok)
+		}
+
+		if !bytes.Equal(info, v.wantData) {
+			t.Fatalf("wanted data: %q, got: %q", v.wantData, info)
+		}
+	}
+}
